f2006abf62de22c0d9894f66b9e7a00860a53000
syqsmyl <gemluo@outlook.com>
2022-10-04 20:45:56 +0800

    使用 webpack-css-extract-plugin 把 CSS 抽成文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
 module.exports = {
@@ -16,12 +17,29 @@ module.exports = {
       title: 'AKI', // 最终会显示在生成的 html 的 title中
       template: 'src/assets/index.html', // html 模板
     }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].[contenthash].css',
+      ignoreOrder: false,
+    }),
   ],
   module: {
     rules: [
       {
-        test: /\.css$/i, // 以 .css 结尾的文件
-        use: ['style-loader', 'css-loader'],
+        // 如果发现了任何以 .css 结尾的文件名，就用 css-loader 将该文件内容读到 js 文件中
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../',
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+        ],
+        // use: ['style-loader', 'css-loader'],
+        // style-loader 会将 css-loader 读到的内容变成 style 标签放到 head 中
       },
     ],
   },
