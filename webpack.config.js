9f0d35a18f926ca0b9d77877bc21d4492a71687d
syqsmyl <gemluo@outlook.com>
2022-10-04 17:01:49 +0800

    用 webpack 生成 html，并引入 css

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
   mode: 'development', // 去除警告，development: 开发者模式，production: 用户模式
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'), // 转义后生成一个 dist 文件夹
-    filename: '[name].[contenthash].js', // 配置缓存
-    // filename: 'index.js', // 转义后的文件命名为 index.js，存放在 dist 文件夹下
+    filename: 'index.[contenthash].js', // 配置缓存
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'AKI', // 最终会显示在生成的 html 的 title中
+      template: 'src/assets/index.html', // html 模板
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i, // 以 .css 结尾的文件
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 }
